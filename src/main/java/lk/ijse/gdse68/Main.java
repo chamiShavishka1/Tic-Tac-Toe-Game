840c5b4343f0e79290511aae6ffe82280520bcbb
chamiShavishka1 <chamishavishka5@gmail.com>
2024-10-26 09:46:58 +0530

    first commit

---
+++ src/main/java/lk/ijse/gdse68/Main.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse68;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
+}
